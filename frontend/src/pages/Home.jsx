062ba0701ed1fdf7e8a7afe79b161efdd5e8485e
wesle@Weiwei <wesleychen56@gmail.com>
2025-03-24 17:36:29 -0400

    Save local changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 function Home() {
-    return <div>Home</div>
+  const navigate = useNavigate();
+
+  return (
+    <div className="container mt-4">
+      <h1>Home</h1>
+      <button
+        className="btn btn-primary mt-3"
+        onClick={() => navigate("/flashcards")}
+      >
+        Go to Flashcards
+      </button>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
